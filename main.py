b1466360f7e793468c5bbe4e17ba335ca82662f8
TheGrabarz <56609226+TheGrabarz@users.noreply.github.com>
2024-01-24 15:39:05 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -97,104 +97,140 @@ async def login(response: Response, request: Request):
         )
 
 @app.post("/update-data")
-async def receive_data(request: Request, tempData: SystemData):
-    app.currentTempValue=round(tempData.temperature,2)
-    app.currentBatteryValue=tempData.battery
-    data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue, "currentTempValue":app.currentTempValue, "currentBatteryValue":app.currentBatteryValue, "minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue, "systemMode":app.systemMode} 
-    return data
+async def receive_data(request: Request, tempData: SystemData, access_token: str = Cookie(None)):
+    if access_token is not None:
+        app.currentTempValue=round(tempData.temperature,2)
+        app.currentBatteryValue=tempData.battery
+        data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue, "currentTempValue":app.currentTempValue, "currentBatteryValue":app.currentBatteryValue, "minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue, "systemMode":app.systemMode} 
+        return data
+    else:
+        redirect_url = request.url_for('login') 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
 
 @app.get("/update-system-mode-on")
-async def update_system_mode(request: Request):
-    redirect_url = request.url_for('home')
-    app.systemMode="On"
-    data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue, "currentTempValue":app.currentTempValue, "currentBatteryValue":app.currentBatteryValue, "minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue, "systemMode":app.systemMode} 
-    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+async def update_system_mode(request: Request, access_token: str = Cookie(None)):
+    if access_token is not None:
+        redirect_url = request.url_for('home')
+        app.systemMode="On"
+        data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue, "currentTempValue":app.currentTempValue, "currentBatteryValue":app.currentBatteryValue, "minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue, "systemMode":app.systemMode} 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+    else:
+        redirect_url = request.url_for('login') 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
 
 @app.get("/update-system-mode-off")
-async def update_system_mode(request: Request):
-    redirect_url = request.url_for('home')
-    app.systemMode="Off"
-    data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue, "currentTempValue":app.currentTempValue, "currentBatteryValue":app.currentBatteryValue, "minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue, "systemMode":app.systemMode} 
-    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+async def update_system_mode(request: Request, access_token: str = Cookie(None)):
+    if access_token is not None:
+        redirect_url = request.url_for('home')
+        app.systemMode="Off"
+        data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue, "currentTempValue":app.currentTempValue, "currentBatteryValue":app.currentBatteryValue, "minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue, "systemMode":app.systemMode} 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+    else:
+        redirect_url = request.url_for('login') 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
 
 @app.get("/update-system-mode-auto")
-async def update_system_mode(request: Request):
-    redirect_url = request.url_for('home')
-    set_system_mode()
-    data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue, "currentTempValue":app.currentTempValue, "currentBatteryValue":app.currentBatteryValue, "minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue, "systemMode":app.systemMode} 
-    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+async def update_system_mode(request: Request, access_token: str = Cookie(None)):
+    if access_token is not None:
+        redirect_url = request.url_for('home')
+        set_system_mode()
+        data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue, "currentTempValue":app.currentTempValue, "currentBatteryValue":app.currentBatteryValue, "minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue, "systemMode":app.systemMode} 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+    else:
+        redirect_url = request.url_for('login') 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
 
 @app.get("/temperature_parameters")
-async def get_temp_limits(request: Request):
-    data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue} 
-    return data
+async def get_temp_limits(request: Request, access_token: str = Cookie(None)):
+    if access_token is not None:
+        data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue} 
+        return data
+    else:
+        redirect_url = request.url_for('login') 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
 
 @app.get("/battery_parameters")
-async def get_battery_limits(request: Request):
-    data={"minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue} 
-    return data
+async def get_battery_limits(request: Request, access_token: str = Cookie(None)):
+    if access_token is not None:
+        data={"minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue} 
+        return data
+    else:
+        redirect_url = request.url_for('login') 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
 
 @app.get("/system-mode")
-async def get_system_mode(request: Request):
-    if("On" in app.systemMode):
-        data={"systemMode": "On"}
+async def get_system_mode(request: Request, access_token: str = Cookie(None)):
+    if access_token is not None:
+        if("On" in app.systemMode):
+            data={"systemMode": "On"}
+        else:
+            data={"systemMode": "Off"}
+        return data
     else:
-        data={"systemMode": "Off"}
-    return data
+        redirect_url = request.url_for('login') 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
 
 @app.post("/update-temp-limits")
-async def update_temp_limits(request: Request, minTemp= Form(None), maxTemp=Form(None)):
-    redirect_url = request.url_for('home')
-    if (minTemp is None or not is_number(minTemp)) and (maxTemp  is None or not is_number(maxTemp)):
+async def update_temp_limits(request: Request, minTemp= Form(None), maxTemp=Form(None), access_token: str = Cookie(None)):
+    if access_token is not None:
+        redirect_url = request.url_for('home')
+        if (minTemp is None or not is_number(minTemp)) and (maxTemp  is None or not is_number(maxTemp)):
+            return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+        if minTemp is None or not is_number(minTemp):
+            minTemp=app.minTempValue
+            if float(maxTemp) < float(app.minTempValue):
+                app.maxTempValue = app.minTempValue
+                app.minTempValue = maxTemp
+                return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER) 
+        if maxTemp is None or not is_number(maxTemp):
+            if float(minTemp) > float(app.maxTempValue):
+                app.minTempValue = app.maxTempValue
+                app.maxTempValue = minTemp
+                return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER) 
+            maxTemp=app.maxTempValue   
+        minTemp=float(minTemp)
+        maxTemp=float(maxTemp)
+        app.minTempValue=round(minTemp,2)
+        app.maxTempValue=round(maxTemp,2)
+        if app.minTempValue > app.maxTempValue:
+            app.minTempValue , app.maxTempValue = app.maxTempValue, app.minTempValue 
+        set_system_mode()
+        data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue, "systemMode":app.systemMode} 
         return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
-    if minTemp is None or not is_number(minTemp):
-        minTemp=app.minTempValue
-        if float(maxTemp) < float(app.minTempValue):
-            app.maxTempValue = app.minTempValue
-            app.minTempValue = maxTemp
-            return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER) 
-    if maxTemp is None or not is_number(maxTemp):
-        if float(minTemp) > float(app.maxTempValue):
-            app.minTempValue = app.maxTempValue
-            app.maxTempValue = minTemp
-            return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER) 
-        maxTemp=app.maxTempValue   
-    minTemp=float(minTemp)
-    maxTemp=float(maxTemp)
-    app.minTempValue=round(minTemp,2)
-    app.maxTempValue=round(maxTemp,2)
-    if app.minTempValue > app.maxTempValue:
-        app.minTempValue , app.maxTempValue = app.maxTempValue, app.minTempValue 
-    set_system_mode()
-    data={"minTempValue": app.minTempValue,  "maxTempValue":app.maxTempValue, "systemMode":app.systemMode} 
-    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+    else:
+        redirect_url = request.url_for('login') 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
 
 @app.post("/update-battery-limits")
-async def update_battery_limits(request: Request, minBattery = Form(None), maxBattery=Form(None)):
-    redirect_url = request.url_for('home')
-    if (minBattery is None or not is_number(minBattery)) and (maxBattery  is None or not is_number(maxBattery)):
+async def update_battery_limits(request: Request, minBattery = Form(None), maxBattery=Form(None), access_token: str = Cookie(None)):
+    if access_token is not None:
+        redirect_url = request.url_for('home')
+        if (minBattery is None or not is_number(minBattery)) and (maxBattery  is None or not is_number(maxBattery)):
+            return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+        if minBattery is None or not is_number(minBattery):
+            minBattery=app.minBatteryValue
+            if float(maxBattery) < float(app.minBatteryValue):
+                app.maxBatteryValue = app.minBatteryValue
+                app.minBatteryValue = maxBattery
+                return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER) 
+        if maxBattery is None or not is_number(maxBattery):
+            if float(minBattery) > float(app.maxBatteryValue):
+                app.minBatteryValue = app.maxBatteryValue
+                app.maxBatteryValue = minBattery
+                return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER) 
+            maxBattery=app.maxBatteryValue   
+        minBattery=float(minBattery)
+        maxBattery=float(maxBattery)
+        app.minBatteryValue=round(minBattery,2)
+        app.maxBatteryValue=round(maxBattery,2)
+        if app.minBatteryValue > app.maxBatteryValue:
+            app.minBatteryValue , app.maxBatteryValue = app.maxBatteryValue, app.minBatteryValue 
+        set_system_mode()
+        data={"minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue, "systemMode":app.systemMode} 
         return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
-    if minBattery is None or not is_number(minBattery):
-        minBattery=app.minBatteryValue
-        if float(maxBattery) < float(app.minBatteryValue):
-            app.maxBatteryValue = app.minBatteryValue
-            app.minBatteryValue = maxBattery
-            return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER) 
-    if maxBattery is None or not is_number(maxBattery):
-        if float(minBattery) > float(app.maxBatteryValue):
-            app.minBatteryValue = app.maxBatteryValue
-            app.maxBatteryValue = minBattery
-            return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER) 
-        maxBattery=app.maxBatteryValue   
-    minBattery=float(minBattery)
-    maxBattery=float(maxBattery)
-    app.minBatteryValue=round(minBattery,2)
-    app.maxBatteryValue=round(maxBattery,2)
-    if app.minBatteryValue > app.maxBatteryValue:
-        app.minBatteryValue , app.maxBatteryValue = app.maxBatteryValue, app.minBatteryValue 
-    set_system_mode()
-    data={"minBatteryValue": app.minBatteryValue,  "maxBatteryValue":app.maxBatteryValue, "systemMode":app.systemMode} 
-    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+    else:
+        redirect_url = request.url_for('login') 
+        return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
 
 def set_system_mode():
     if((app.currentTempValue < app.maxTempValue) and (app.currentTempValue > app.minTempValue) and (app.currentBatteryValue <app.maxBatteryValue) and (app.currentBatteryValue > app.minBatteryValue)):
